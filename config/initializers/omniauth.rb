1ed05c1ac334e26b9de06fab6d439614c723f08a
ilyes mathieu <ilyes.mathieu@gmail.com>
2013-05-13 18:12:14 +0200

    init2

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-Omniauth.config.logger = Rails.logger
+#Omniauth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  #provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, 'FACEBOOK_APP_ID', 'FACEBOOK_SECRET'
 end
