a61923059176c7d3921794c2a075f8eadcc2d3ed
ilyes mathieu <ilyes.mathieu@gmail.com>
2013-05-15 15:46:51 +0200

    ok full auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :facebook, '554088804642131', '3476610a22b5f0eaaccb4686afdd1163'
   provider :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
-  provider :identity
+  provider :identity, on_failed_registration: lambda{ |env|
+		IdentitiesController.action(:new).call(env)
+  }
+  
+  
 end
